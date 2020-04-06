7fdada6202f6b172c6f20aa5c2102ac65d070970
peekinternational <peekinternational@gmail.com>
2020-04-06 16:51:50 +0500

    email

--- secure/resources/views/forget_password.blade.php
+++ secure/resources/views/forget_password.blade.php
@@ -49,6 +49,12 @@ background-repeat: no-repeat;
 .pt-20 {
  padding-top:20px;
 }
+.login_forms .form-control {
+    min-height: calc(1.9em + .75rem + 5px);
+    box-shadow: inset 0px -1px 1px -2px rgb(149, 149, 149);
+    border-radius: 6px;
+    font-size: 16px;
+}
 </style>
     <div class="welcome overflow-hidden">
         <div class="container h-100 position-relative">
@@ -65,15 +71,25 @@ background-repeat: no-repeat;
                 <div class="col-md-6">
                     <div class="row" class="set-login">
                         <div class="col-md-9 mx-auto pt-5">
-                            @if(session()->has('fail_login'))
+                          @if(Session::has('resetAlert'))
                             <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                <strong>Warning!</strong> {!! session()->get('fail_login') !!}
+                                <strong>Warning!</strong> {!! session()->get('resetAlert') !!}
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                                {{ session()->forget('fail_login') }}
+                                {{ session()->forget('resetAlert') }}
                             @endif
+                          @if(Session::has('resetSuccess'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <strong>Warning!</strong> {!! session()->get('resetSuccess') !!}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                                {{ session()->forget('resetSuccess') }}
+                            @endif
+
                             @if($errors->any())
                                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
                                         <ul class="mb-0">
@@ -88,32 +104,17 @@ background-repeat: no-repeat;
                                         </button>
                                     </div>
                             @endif
-                            <form method="post" class="login_form" action="">
+                            <form method="post" class="login_forms" action="{{ url('/checkEmail') }}">
                                 {!! csrf_field() !!}
                                 <div class="form-group">
-                                    <label>Username</label>
-                                    <input class="form-control" type="text" required name="username">
+                                    <label>Email</label>
+                                    <input type="email" class="form-control" placeholder="Enter email" id="email" name="email">
                                 </div>
                                 <div class="form-group">
-                                    <label>Password</label>
-                                    <input class="form-control" type="password" required name="password">
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                    <button class="btn btn-primary btn-block" type="submit">Submit</button>
                                 </div>
                             </form>
-                            <a href="{{url('forget-password')}}" style="color:white;">Forget password ?</a>
-                            <p class="text-center mt-4 mb-4"><a class="btn btn-register btn-block" href="{!! route('register') !!}">Register</a></p>
-                            @if(setting('social_login'))
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <a href="{!! route('loginfacebook') !!}" class="btn btn-block btn-facebook btn-sm mb-2"><i class="fab fa-facebook-f"></i> Login with Facebook</a>
-                                </div>
-                                <div class="col-md-6">
-                                    <a href="{!! route('logintwitter') !!}" class="btn btn-block btn-twitter btn-sm mb-2"><i class="fab fa-twitter"></i> Login with Twitter</a>
-                                </div>
-                            </div>
-                            @endif
+
                         </div>
                     </div>
                 </div>
---
+++ secure/resources/views/photo/view2.blade.php
@@ -0,0 +1,127 @@
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    @foreach($photos->chunk(1) as $key => $chunk)
+    @foreach($chunk as $photo)
+    <div class="carousel-item active">
+      <div class="row position-relative">
+          <span class="close-view-photo position-absolute">&times;</span>
+          <div class="col-md-8">
+            <?php
+            $url = url()->full();
+            $url2 = substr($url,7,9);
+            if ($url2 == 'localhost') {
+              if ($type == 'cover') {
+                $photo_file = avatar($photo->avatar, $photo->gender);
+                $photo_file = substr($photo_file,34);
+                $photo_file ='http://localhost/dating/'.$photo_file;
+              }else {
+                $photo_file = $photo->file;
+                $photo_file ='http://localhost/dating/'.$photo_file;
+              }
+            }else {
+              if ($type == 'cover') {
+                $photo_file = avatar($photo->avatar, $photo->gender);
+              }else {
+                $photo_file = $photo->file;
+              }
+              // dd($photo_file);
+            }
+             ?>
+              <!-- <div class="full_photo d-flex align-items-center"><img src="{!! url($photo_file) !!}" style="width:100%;max-height: 630px;min-height:500px;"></div> -->
+              <div class="full_photo d-flex align-items-center"><img src="{!! url($photo_file) !!}" style="width:100%;max-height: 570px;min-height:450px;"></div>
+          </div>
+          <div class="col-md-4 view-photo-right">
+              <div class="content-photo">
+                  <div class="user-info">
+                      <div class="media">
+                        <?php
+                        if($type == 'cover') {
+                          $avatar = avatar($photo->avatar, $photo->gender);
+                          $username = $photo->username;
+                          $firstname = $photo->firstname;
+                          $lastname = $photo->lastname;
+                          $user_id = $photo->id;
+                          // print_r($username); die;
+                        }else {
+                          $avatar = avatar($photo->user->avatar, $photo->user->gender);
+                          $username = $photo->user->username;
+                          $firstname = $photo->user->firstname;
+                          $lastname = $photo->user->lastname;
+                          $user_id = $photo->user->id;
+                        }
+                        // dd($avatar);
+                        $url = url()->full();
+                        $url2 = substr($url,7,9);
+                        if ($url2 == 'localhost') {
+                          $avatar = substr($avatar,34);
+                          $avatar ='http://localhost/dating/'.$avatar;
+                          // dd($avatar);
+                        }
+                        // dd(auth()->user()->follows()->get()->pluck('id'));
+                        // dd(in_array($user_id, collect(auth()->user()->follows()->get())->pluck('id')->all()));
+                        ?>
+                          <img src="{!! $avatar !!}">
+                          <div class="media-body">
+                              <h3 class="mb-0"><a href="{!! route('profile',['username'=>$username]) !!}">{!! fullname($firstname, $lastname, $username) !!}</a></h3>
+                                @if(auth()->check() && in_array($user_id, collect(auth()->user()->follows()->get())->pluck('id')->all()))
+                                    <span data-id="{!! $user_id !!}" class="badge badge-primary btn-follow"><i class="fas fa-check"></i> Followed</span>
+                                @else
+                                    <span data-id="{!! $user_id !!}" class="badge badge-primary btn-follow">Follow</span>
+                                @endif
+                                @if($type != 'cover')
+                                <p>
+                                    {!! $photo->description !!}
+                                </p>
+                              @endif
+                          </div>
+                      </div>
+                  </div>
+                  <div class="comments">
+                      <ul class="list-unstyled m-0">
+                        @if($type != 'cover')
+                          @if($photo->comments()->count())
+                              @foreach($photo->comments as $comment)
+                                  @include('photo.comment')
+                              @endforeach
+                          @endif
+                        @endif
+                      </ul>
+                  </div>
+              </div>
+              @if($type != 'cover')
+              <div class="photo-action">
+                  <div class="like-photo clearfix">
+                      @if($photo->likes()->count() && auth()->check() && in_array(auth()->id(), collect($photo->likes()->get())->pluck('id')->all()))
+                          <i class="fas fa-heart" data-id="{!! $photo->id !!}" id="love-photo"></i>
+                      @else
+                          <i class="far fa-heart" data-id="{!! $photo->id !!}" id="love-photo"></i>
+                      @endif
+                      <span>{!! $photo->likes()->count()?$photo->likes()->count():'' !!}</span>
+                  </div>
+                  <textarea data-id="{!! $photo->id !!}" class="write-comment" placeholder="Comment on this photo"></textarea>
+              </div>
+              @endif
+
+          </div>
+      </div>
+      <!-- <img src="http://localhost/dating/uploads/photos/13/badJdQvNgH1Xfy7ToDRf1585045832_thumb.jpg" alt="Los Angeles" width="1100" height="500"> -->
+    </div>
+    @endforeach
+    @endforeach
+    <!-- <div class="carousel-item">
+      <img src="http://localhost/dating/uploads/photos/13/QZoUqO0dkTEaz6ipxwL81583950232_thumb.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost/dating/uploads/photos/13/zl90b4phkzXvv2uJ4Bd11582556885_thumb.jpg" alt="New York" width="1100" height="500">
+    </div> -->
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
